1af43fcf97833d855895f3b7539233dd7b10a005
Kaktus200020 <artemkremnev2007@gmail.com>
2023-01-29 16:07:14 +0300

    Serialization

--- TCP_Chat_Client/Program.cs
+++ TCP_Chat_Client/Program.cs
@@ -5,6 +5,9 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace TCP_Chat_Client
 {
@@ -20,20 +23,65 @@ namespace TCP_Chat_Client
             Console.WriteLine("Нажмите Энтер для подключения");
             Console.ReadLine();
             socket_sender.Connect(endPoint);
+            Console.WriteLine("Введите Имя: ");
+            string name = Console.ReadLine();
+            SendMessage(socket_sender, name);
+            Action<Socket> taskSendMessage = SendMessageForTask;
+            taskSendMessage.BeginInvoke(socket_sender, null, null);
+            
+            
             while(true)
-            {
-                Console.WriteLine("Введите сообщение: ");
-                string message = Console.ReadLine();
-                byte[] bytes = Encoding.Unicode.GetBytes(message);
-                socket_sender.Send(bytes);
-                Console.WriteLine("Посылка \"" + message + "\" отправлена");
-                byte[] byte_answers = new byte[1024];
-                int num_bytes=socket_sender.Receive(byte_answers);
-                string answer=Encoding.Unicode.GetString(byte_answers, 0, num_bytes);
-                Console.WriteLine(answer);
-                Console.WriteLine();
+            { 
+                string answer = GetMessage(socket_sender);
+                Console.WriteLine(answer);  
             }
             Console.ReadLine();
+        }
+        public static void SendMessageForTask(Socket socket)
+        {
+            while(true)
+            {
+                string message = Console.ReadLine();
+                if(message=="platypus")
+                {
+                    Platypus platypus = new Platypus()
+                    {
+                        Size = 2,Color="Brown"
+                        
+                    };
+                    XmlSerializer xmlData = new XmlSerializer(typeof(Platypus));
+                    MemoryStream stream = new MemoryStream();
+                    xmlData.Serialize(stream, platypus);
+                    stream.Position = 0;
+                    Platypus platypus2 = xmlData.Deserialize(stream) as Platypus;
+                    byte[] bytes = stream.ToArray();
+                    //SendMessage(socket, message);
+                    socket.Send(bytes);
+                }
+                if(message=="dumpling")
+                {
+                    Dumpling dumpling = new Dumpling() {IsFried=true , Name="Иван", Description="Его имя Иван, он крутой" };
+                    string text=JsonConvert.SerializeObject(dumpling);
+                    SendMessage(socket, text);
+                }
+                else
+                {
+                    SendMessage(socket, message);
+                }
+                
+            }
+        }
+        public static void SendMessage(Socket socket, string message)
+        {
+            byte[] bytes_answer = Encoding.Unicode.GetBytes(message);
+            socket.Send(bytes_answer);
+        }
+        public static string GetMessage(Socket socket)
+        {
+            byte[] bytes = new byte[1024];
+            int num_bytes = socket.Receive(bytes);
+            return Encoding.Unicode.GetString(bytes, 0, num_bytes);
+
         }
     }
 }
--- TCP_Chat_Server/Program.cs
+++ TCP_Chat_Server/Program.cs
@@ -5,11 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
+using System.Xml.Serialization;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace TCP_Chat_Server
 {
     class Program
     {
+        static string messageFromUser = "";
+        static List<Socket> sockets = new List<Socket>();
         static void Main(string[] args)
         {
             Console.Title = "Server";
@@ -18,25 +24,102 @@ namespace TCP_Chat_Server
             IPAddress address = IPAddress.Parse("127.0.0.1");
             IPEndPoint endPoint = new IPEndPoint(address, 7632);
             socket.Bind(endPoint);
-            socket.Listen(1);
+            socket.Listen(2);
             Console.WriteLine("Ожидаем звонка от клиента");
-            Socket socket_client = socket.Accept();
-            Console.WriteLine("Клиент на связи");
-            while(true)
+            while (true)
             {
-                byte[] bytes = new byte[1024];
-                int num_bytes = socket_client.Receive(bytes);
-                string textFromClient = Encoding.Unicode.GetString(bytes, 0, 20);
-                Console.WriteLine(textFromClient);
+                Socket socket_client = socket.Accept();
+                sockets.Add(socket_client);
+                Console.WriteLine("Клиент на связи");
+                Thread threadReceive = new Thread(GetMessageForManager);
+                threadReceive.Start(socket_client);
+                Thread threadSend = new Thread(SendMessageForManager);
 
-                // ответное сообщение от сервера к клиенту 
-                string answer = "SERVER: OK";
-                byte[] bytes_answer = Encoding.Unicode.GetBytes(answer);
-                socket_client.Send(bytes_answer);
-                 
+                threadSend.Start(socket_client);
             }
-            
+
+
+
             Console.ReadLine();
         }
+        public static void SendMessage(Socket socket, string message)
+        {
+            byte[] bytes_answer = Encoding.Unicode.GetBytes(message);
+            socket.Send(bytes_answer);
+        }
+        public static string GetMessage(Socket socket)
+        {
+            byte[] bytes = new byte[1024];
+            int num_bytes = socket.Receive(bytes);
+            return Encoding.Unicode.GetString(bytes, 0, num_bytes);
+
+        }
+        public static void GetMessageForManager(object socketObj)
+        {
+            Socket socket = (Socket)socketObj;
+            string name = GetMessage(socket);
+            while (true)
+            {
+                messageFromUser = GetMessage(socket);
+                Console.WriteLine("[" + name + "]: " + messageFromUser);
+                //Раскодировать сообщение от пользователя
+                //ProcessCommandWord(socket,messageFromUser);
+                //ProcessCommandCoding(socket, messageFromUser);
+                //byte[] bytes = new byte[1024];
+                //int num_bytes = socket.Receive(bytes);
+                //ProcessCommandXML (socket, bytes, num_bytes);
+                ProcessCommandJson(socket, messageFromUser);
+            }
+
+        }
+        public static void SendMessageToAllUsers(string message)
+        {
+            foreach (var socket in sockets)
+            {
+                SendMessage(socket, message);
+            }
+        }
+        public static void SendMessageForManager(object socketObj)
+        {
+            while (true)
+            {
+                Socket socket = (Socket)socketObj;
+                string sendMessage = Console.ReadLine();
+
+                SendMessageToAllUsers(sendMessage);
+
+            }
+        }
+        private static void ProcessCommandXML(Socket socket, byte[] bytes, int num_bytes)
+        {
+            XmlSerializer xmlData = new XmlSerializer(typeof(Platypus));
+           
+            MemoryStream stream = new MemoryStream(bytes,0,num_bytes);
+            stream.Position = 0;
+            Platypus platypus2 = xmlData.Deserialize(stream) as Platypus;
+        }
+        private static void ProcessCommandJson(Socket socket, string text)
+        {
+            Dumpling dumpling=JsonConvert.DeserializeObject<Dumpling>(text);
+        }
+        private static void ProcessCommandWord(Socket socket, string command)
+        {
+            if (messageFromUser == "color")
+            {
+                Console.WriteLine("Пользователь прислал команду color");
+                SendMessage(socket, "сервер принял вашу команду");
+            }
+        }
+        private static void ProcessCommandCoding(Socket socket, string text)
+        {
+            int health, level, money;
+            string[] numsText =text.Split(',');
+            health = int.Parse(numsText[0]);
+            level = int.Parse(numsText[1]);
+            money = int.Parse(numsText[2]);
+            Console.WriteLine($"Health:{health}, level:{level},money:{money}");
+        }
     }
+    
+
 }
